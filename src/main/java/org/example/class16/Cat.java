352ece10be43b04271594af406e87207bc08cc6e
AlexAlbarran <alejandro.albarran.01@gmail.com>
2024-04-03 21:37:38 -0500

    Updated file

---
+++ src/main/java/org/example/class16/Cat.java
@@ -0,0 +1,16 @@
+package org.example.class16;
+
+import org.example.class17.Animal;
+
+public class Cat extends Animal {
+
+    void speak(){
+        System.out.println("Meow(Java) Meow(Java)");
+    }
+
+    void printInfo(){
+        System.out.println(name);
+    }
+
+
+}
